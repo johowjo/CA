7611ff0f86b78002d96099a3f7fa60396ac79d4a
johowjo <johowjo@gmail.com>
2024-12-23 06:30:49 +0800

    architecture correct, remain muldiv

--- final_project/CPU.v
+++ final_project/CPU.v
@@ -1,41 +1,42 @@
 // Your code
-module FSM(clk, rst_n, next, state);
-input clk, rst_n, in;
-output out;
+module FSM(clk, rst_n, next, out);
+  input clk, rst_n, next;
+  output [4:0] out;
+  localparam ID = 5'd0;
+  localparam IF = 5'd1;
+  localparam EX = 5'd2;
+  localparam MEM = 5'd3;
+  localparam MEM_WAIT = 5'd4;
+  localparam WB = 5'd5;
+  localparam PC = 5'd6;
 
-localparam ID = 5'd0;
-localparam IF = 5'd1;
-localparam EX = 5'd2;
-localparam MEM = 5'd3;
-localparam WB = 5'd4;
+  reg [4:0] state, state_next;
+  assign out = state;
 
-reg [4:0] state, state_next;
-assign out = state;
-
-always @(*) begin
-  case(state)
-    ID: state_next = next ? ID : IF;
-    IF: state_next = next ? IF : EX;
-    EX: state_next = next ? EX : MEM;
-    MEM: state_next = next ? MEM : WB;
-    WB: state_next = next ? WB : ID;
+  always @(*) begin
+    case(state)
+      ID: state_next = next ? IF : ID;
+      IF: state_next = next ? EX : IF;
+      EX: state_next = next ? MEM : EX;
+      MEM: state_next = next ? MEM_WAIT : MEM;
+      MEM_WAIT: state_next = next ? WB : MEM_WAIT;
+      WB: state_next = next ? PC : WB;
+      PC: state_next = next ? ID : PC;
+    endcase
   end
-end
 
-always @(posedge clk or negedge rst_n) begin
-  if(!rst_n) begin
-    state <= ID;
-  end else begin
-    state <= state_next;
+  always @(posedge clk or negedge rst_n) begin
+    //$display(out);
+    //$display(state);
+    if(!rst_n) begin
+      state <= ID;
+    end else begin
+      state <= state_next;
+    end
   end
-end
 
 endmodule
 
-module CU(clk, rst_n, opc, out);
-input clk, rsrt_n, opt;
-output out;
-endmodule
 
 module CPU(clk,
             rst_n,
@@ -62,7 +63,7 @@ module CPU(clk,
     // Do not modify this part!!!            //
     // Exception: You may change wire to reg //
     reg    [31:0] PC          ;              //
-    wire   [31:0] PC_nxt      ;              //
+    reg    [31:0] PC_nxt      ;              //
     wire          regWrite    ;              //
     wire   [ 4:0] rs1, rs2, rd;              //
     wire   [31:0] rs1_data    ;              //
@@ -89,20 +90,32 @@ module CPU(clk,
     localparam mul = 5'd15;
     localparam div = 5'd16;
     localparam remu = 5'd17;
+    localparam sw = 5'd18;
+    localparam add = 5'd19;
 
     // Todo: other wire/reg
     reg [4:0] mode;
     reg [31:0] instruction;
-    reg [6:0] opcode;
-    reg [2:0] func3;
-    reg [6:0] func7;
+    wire [6:0] opcode;
+    //reg [6:0] opcode;
+    wire [2:0] func3;
+    wire [6:0] func7;
     reg [31:0] alu_lhs;
     reg [31:0] alu_rhs;
     reg [31:0] alu_result;
     reg [31:0] alu_result_next;
     reg [31:0] wb_data;
     reg [31:0] wb_data_next;
+    wire [4:0] state;
+    reg wen_d;
+    reg next;
+    reg regwrite;
 
+    FSM fsm(
+      .clk(clk),
+      .rst_n(rst_n),
+      .next(next),
+      .out(state));
 
     //---------------------------------------//
     // Do not modify this part!!!            //
@@ -121,21 +134,25 @@ module CPU(clk,
     // Todo: any combinational/sequential circuit
 
     assign addr_I = PC;
+    assign wen_D = wen_d;
     assign opcode = instruction[6:0];
     assign func3 = instruction[14:12];
     assign func7 = instruction[31:25];
     assign rs1 = instruction[19:15];
     assign rs2 = instruction[24:20];
-    assign rs = instruction[11:7];
+    assign rd = instruction[11:7];
     assign addr_D = alu_result;
     assign wdata_D = rs2_data;
+    assign rd_data = wb_data;
+    assign regWrite = regwrite;
 
     always @(*) begin
+      //opcode = instruction[14:12];
       // parsing mode
       case(opcode) 
         7'd3: mode = lw;
         7'd19: begin
-          case(func3) begin
+          case(func3) 
             3'd0: mode = addi;
             3'd1: mode = slli;
             3'd2: mode = slti;
@@ -143,99 +160,231 @@ module CPU(clk,
               case(func7) 
                 7'd0: mode = srli;
                 7'd32: mode = srai;
-              end
+              endcase
             end
-          end
+          endcase
         end
         7'd23: mode = auipc;
-        7'd35: mode: sw;
+        7'd35: mode = sw;
         7'd51: begin
           case(func3)
             3'd0: begin
               case(func7)
-                7'd0: add;
-                7'd32: sub;
-              end
+                7'd0: mode = add;
+                7'd1: mode = mul;
+                7'd32: mode = sub;
+              endcase
             end
-          end
+            3'd5: mode = div;
+            3'd7: mode = remu;
+          endcase
         end
         7'd55: mode = lui;
         7'd99: begin
           case(func3)
             3'd0: mode = beq;
             3'd1: mode = bne;
-            3'd2: mode = blt;
-            3'd3: mode = bge;
-          end
+            3'd4: mode = blt;
+            3'd5: mode = bge;
+          endcase
         end
         7'd103: mode = jalr;
         7'd111: mode = jal;
-      end
+      endcase
       // alu
       case(mode)
         lw: begin
-          alu_result_next = rs1_data + instruction[31:20];
+          alu_result_next = rs1_data + {{21{instruction[31]}}, instruction[30:20]};
+          PC_nxt = PC + 4;
+          wb_data_next = rdata_D;
         end
         sw: begin
-          alu_result_next = rs1_data + {instruction[31:20], instruction[11:7]};
+          alu_result_next = rs1_data + {{21{instruction[31]}}, instruction[30:25], instruction[11:7]};
+          PC_nxt = PC + 4;
         end
         add: begin
           alu_result_next = rs1_data + rs2_data;
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         sub: begin
-          alu_result_next = rs1_data - rs2_data;
+          alu_result_next = $signed(rs1_data) - $signed(rs2_data);
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
+        end
+        addi: begin
+          alu_result_next = rs1_data + {{21{instruction[31]}}, instruction[30:20]};
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         slli: begin
-          alu_result_next = rs1_data << {19'd0, instruction[31:20]};
+          alu_result_next = rs1_data << instruction[24:20];
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         srli: begin
-          alu_result_next = rs1_data >> {19'd0, instruction[31:20]};
+          alu_result_next = rs1_data >> instruction[24:20];
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         srai: begin
-          alu_result_next = rs1_data >>> {19'd0, instruction[31:20]};
+          alu_result_next = rs1_data >>> instruction[24:20];
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         slti: begin
-          alu_result_next = (rs1_data < {19'd0, instruction[31:20]}) ? 32'b1 : 32'b0;
+          alu_result_next = ($signed(rs1_data) < $signed(instruction[31:20])) ? 32'b1 : 32'b0;
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         beq: begin
           alu_result_next = (rs1_data == rs2_data) ? 32'b1: 32'b0;
+          if(rs1_data == rs2_data) begin
+            PC_nxt = PC + {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+          end else begin
+            PC_nxt = PC + 4;
+          end
+        end
+        bge: begin
+          alu_result_next = ($signed(rs1_data) >= $signed(rs2_data)) ? 32'b1: 32'b0;
+          if($signed(rs1_data) >= $signed(rs2_data)) begin
+            PC_nxt = PC + {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+          end else begin
+            PC_nxt = PC + 4;
+          end
+        end
+        blt: begin
+          alu_result_next = (rs1_data < rs2_data) ? 32'b1: 32'b0;
+          if($signed(rs1_data) < $signed(rs2_data)) begin
+            PC_nxt = PC + {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+          end else begin
+            PC_nxt = PC + 4;
+          end
         end
         bne: begin
           alu_result_next = (rs1_data != rs2_data) ? 32'b1: 32'b0;
+          if(rs1_data != rs2_data) begin
+            PC_nxt = PC + {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+          end else begin
+            PC_nxt = PC + 4;
+          end
         end
         jalr: begin
           alu_result_next = PC + 4;
+          PC_nxt = rs1_data + {{21{instruction[31]}}, instruction[30:20]};
+          //PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         jal: begin
           alu_result_next = PC + 4;
+          //PC_nxt = PC + $signed({instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0});
+          PC_nxt = PC + {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+          wb_data_next = alu_result;
         end
         auipc: begin
           alu_result_next = PC + {instruction[31:12], 12'b0};
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         lui: begin
           alu_result_next = {instruction[31:12], 12'b0};
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         mul: begin
-          alu_result_next = (rs1_data * rs2_data) & 32'hffffffff;
+          //alu_result_next = (rs1_data * rs2_data) & 32'hffffffff;
+          alu_result_next = $signed(rs1_data) * $signed(rs2_data);
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         div: begin
           alu_result_next = rs1_data / rs2_data;
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
         remu: begin
           alu_result_next = rs1_data % rs2_data;
+          PC_nxt = PC + 4;
+          wb_data_next = alu_result;
         end
-      end
+      endcase
     end
 
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             PC <= 32'h00010000; // Do not modify this value!!!
-            
+            next <= 1;
+            wen_d <= 0;
+            regwrite <= 0;
+            if(mode == sw) begin
+              wen_d <= 0;
+            end
         end
         else begin
-            PC <= PC_nxt;
-            
+            //PC <= PC_nxt;
+            next <= 1;
+            case(state)
+              5'd0: begin
+                instruction <= rdata_I;
+                regwrite <= 0;
+              end
+              5'd1: begin
+                
+                if(mode == srai) begin
+                  $display("%h", rs1_data);
+                  $display("%d", instruction[31:20]);
+                end
+                
+                //$display("%h", rs1);
+                //$display("%h", rs1_data);
+                //$display("%h", mode);
+                //$display("%h", addr_I);
+                //$display("%h", PC);
+              end
+              5'd2: begin
+                //$display("%d", mode);
+                //$display("%h", alu_result_next);
+                alu_result <= alu_result_next;
+              end
+              5'd3: begin
+                //$display(mode);
+                if(mode == sw) begin
+                  wen_d <= 1;
+                end
+              end
+              5'd4: begin
+                wb_data <= wb_data_next;
+              end
+              5'd5: begin
+                //$display("foo");
+                //PC <= PC_nxt;
+                //$display(PC);
+                wen_d <= 0;
+                case(mode)
+                  lw: regwrite <= 1;
+                  add: regwrite <= 1;
+                  sub: regwrite <= 1;
+                  addi : regwrite <= 1;
+                  slli : regwrite <= 1;
+                  srli : regwrite <= 1;
+                  srai : regwrite <= 1;
+                  slti : regwrite <= 1;
+                  jal : regwrite <= 1;
+                  jalr : regwrite <= 1;
+                  lui : regwrite <= 1;
+                  mul: regwrite <= 1;
+                  div: regwrite <= 1;
+                  remu: regwrite <= 1;
+                  auipc: regwrite <= 1;
+                  default: regwrite <= 0;
+                endcase
+              end
+              5'd6: begin
+                //$display(PC);
+                PC <= PC_nxt;
+              end
+            endcase
         end
     end
 endmodule
@@ -285,7 +434,8 @@ module reg_file(clk, rst_n, wen, a1, a2, aw, d, q1, q2);
     end
 endmodule
 
+/*
 module mulDiv(clk, rst_n, valid, ready, mode, in_A, in_B, out);
     // Todo: your HW2
-
 endmodule
+*/
